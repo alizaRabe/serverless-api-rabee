2621364e189b5e9f44a0333b79f3ae07699a9733
Nielven Mascariñas <mascarinas022@gmail.com>
2024-04-19 00:06:24 +0800

    fix path error in models folder

--- functions/models/author.js
+++ functions/models/author.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const authorSchema = require("../schemas/author");
+const authorSchema = require("../schema/author");
 
 const AuthorModel = mongoose.model("Author", authorSchema);
 
